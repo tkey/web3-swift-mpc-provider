e20fb9ad43174b146101e5fb61a9d28c62ee152d
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-10-05 07:58:20 +0200

    fix: update dependency

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/argentlabs/web3.swift", from:"1.6.0"),
-        .package(url: "https://github.com/torusresearch/tss-client-swift.git", from: "1.0.14"),
+        .package(url: "https://github.com/torusresearch/tss-client-swift.git", from: "1.0.15"),
     ],
     targets: [
         .target(
